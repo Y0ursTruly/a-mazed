3882ca86b6d3afdc02e10eba004957899a1e44a8
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2025-03-09 19:19:10 -0400

    Update create_server.js

--- create_server.js
+++ create_server.js
@@ -12,8 +12,8 @@ module.exports=function create_server(responder,options={}){
   tls_key ||= process.env.tls_key || process.env.TLS_KEY
   tls_cert ||= process.env.tls_cert || process.env.TLS_CERT
   const ownsCert=tls_key&&tls_cert
-  const key=typeof tls_key==="string"? fs.readFileSync(tls_key): tls_key
-  const cert=typeof tls_cert==="string"? fs.readFileSync(tls_cert): tls_cert
+  let key=typeof tls_key==="string"? fs.readFileSync(tls_key): tls_key
+  let cert=typeof tls_cert==="string"? fs.readFileSync(tls_cert): tls_cert
   const envPort=process.env.port||process.env.PORT
   const PORT=envPort? Number(envPort): (typeof options==="number"?options:(options.port||options.PORT))
   const server=ownsCert?
@@ -22,5 +22,28 @@ module.exports=function create_server(responder,options={}){
   server.listen(PORT||(ownsCert?443:80), function(){
     console.log(`hosting http${ownsCert?'s':''} server @ PORT ${server.address().port}`)
   })
+  if(!options.key_renewer){
+    options.key_renewer = typeof tls_key==="string"?
+      function(){return fs.readFileSync(tls_key)}:
+      function(){return tls_key}
+  }
+  if(!options.cert_renewer){
+    options.cert_renewer = typeof tls_cert==="string"?
+      function(){return fs.readFileSync(tls_cert)}:
+      function(){return tls_cert}
+  }
+  if(ownsCert){
+    const {key_renewer,cert_renewer}=options
+    //every 60 seconds check to renew certificate data
+    setInterval(async function(){
+      //the key_renewer and cert_renewer if given can be asynchronous
+      const key_next = await key_renewer()
+      const cert_next = await cert_renewer()
+      if(key_next===key && cert_next===cert) return null; //no changes to be made
+      key = key_next
+      cert = cert_next
+      server.setSecureContext({key,cert})
+    },6e4)
+  }
   return server
 }
